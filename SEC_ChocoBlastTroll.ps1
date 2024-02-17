1de74980e2b67a45f9a1b793d5ba4dd43fb6458c
43Er32x <160287229+43Er32x@users.noreply.github.com>
2024-02-17 12:21:59 +0100

    Add files via upload

---
+++ SEC_ChocoBlastTroll.ps1
@@ -0,0 +1,23 @@
+﻿# Fonction pour vérifier le mouvement de la souris
+function CheckMouseMovement {
+    # Récupérer la position initiale du curseur
+    $initialPosition = [System.Windows.Forms.Cursor]::Position
+
+    # Attendre quelques secondes pour laisser l'utilisateur bouger la souris
+    Start-Sleep -Seconds 5
+
+    # Récupérer la nouvelle position du curseur
+    $newPosition = [System.Windows.Forms.Cursor]::Position
+
+    # Vérifier si les positions sont différentes
+    if ($initialPosition -ne $newPosition) {
+        # Si le curseur a bougé, verrouiller la session utilisateur
+        rundll32.exe user32.dll,LockWorkStation
+    }
+}
+
+# Boucle infinie pour vérifier le mouvement de la souris
+while ($true) {
+    CheckMouseMovement
+    exit 0
+}
